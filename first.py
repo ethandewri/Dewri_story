85915913b5169e05669412b05cef980f7e703c2a
Ethan Dewri <ethandew.21598@redlandsschools.net>
2018-09-10 15:24:39 +0000

    hello

---
+++ first.py
@@ -0,0 +1,3 @@
+# Write a program that will add six to the users input and return he result!
+num = int(input('Please enter a number '))
+print (num +6)
---
+++ second.py
@@ -0,0 +1,1 @@
+# Write a program that will ask the user what their age is and then determine if they are old enpugh to vote or not and respond 
